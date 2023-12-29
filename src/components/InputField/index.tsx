36ccbf50e44ebcff81ea3d3d42fcb40e1f082de3
KirillTovpinets <kirill.tovpinets@icloud.com>
2023-12-29 22:41:02 +0300

    fix base url

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,11 +1,9 @@
 import { FormGroup, Input } from "@mui/material";
-import CopyButton from "../CopyButton";
 
 function InputField({ value }: { value: string }) {
   return (
     <FormGroup>
       <Input value={value} />
-      <CopyButton text={value} />
     </FormGroup>
   );
 }
